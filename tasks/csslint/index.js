f1df2c890e12fa7f8dff3c69623c85b8812071a2
Christian Hanne <support@aureola.codes>
2016-05-01 01:03:40 +0200

    Updated csslint logic.

--- tasks/csslint/index.js
+++ tasks/csslint/index.js
@@ -5,10 +5,34 @@
  */
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const exec = require('child_process').exec;
 const config = require('./package.json').config;
 
-exec(`./node_modules/.bin/csslint ${config.src}`, (error, stdout, stderr) => {
+const rulesRaw = fs.readFileSync(path.resolve(__dirname, '.csslintrc'), 'utf8');
+const rules = JSON.parse(rulesRaw);
+
+let errors = [];
+let ignore = [];
+for (let key of Object.keys(rules)) {
+  if (rules[key] === true) {
+    errors.push(key);
+  }
+  else {
+    ignore.push(key);
+  }
+}
+
+let command = ([
+  './node_modules/.bin/csslint',
+  '--quiet',
+  '--errors="' + errors.join(',') + '"',
+  '--ignore="' + ignore.join(',') + '"',
+  config.src
+]).join(' ');
+
+exec(command, (error, stdout, stderr) => {
   if (stdout || stderr || error) {
     console.log(stdout + stderr + error);
   }
